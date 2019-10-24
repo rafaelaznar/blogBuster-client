e6c7d61eb9646e7eaa49e4fef60ee60c7c4e13f0
pixeled4life <cozahmiah2@gmail.com>
2019-10-24 13:09:18 +0200

    Quick changes

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,7 @@
 miModulo.config(['$routeProvider',function($routeProvider){
 
     $routeProvider.when('/',{templateUrl:'app/homeTemplate.html',controller:'homeController'})
-    $routeProvider.when('/post/plist/:rpp/:page',{templateUrl:'app/post/plist.html',controller:'postPlistController'})
+    $routeProvider.when('/post/plist/:rpp/:page',{templateUrl:'app/post/plist/plist.html',controller:'postPlistController'})
 
     $routeProvider.otherwise({redirectTo:'/'})
 
