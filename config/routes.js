0ea4576d09560b1d58011c656ab613e90782f528
Rafael Aznar <rafaaznar@gmail.com>
2019-10-24 12:32:55 +0200

    plist y botonera inacabada de clase

--- config/routes.js
+++ config/routes.js
@@ -5,6 +5,8 @@ miModulo.config(['$routeProvider',function($routeProvider){
     $routeProvider.when('/multi',{templateUrl:'app/multiplicacionTemplate.html',controller:'multiplicacionController'})
     $routeProvider.when('/suma',{templateUrl:'app/sumaTemplate.html',controller:'sumaController'})
 
+    $routeProvider.when('/post/plist/:rpp/:page',{templateUrl:'app/post/plist.html',controller:'postPlistController'})
+
     $routeProvider.otherwise({redirectTo:'/'})
 
 
