bd82f077223a3537be17594d8e709817c2e231d7
pixeled4life <cozahmiah2@gmail.com>
2019-10-24 12:57:07 +0200

    Pulled changes from upstream

--- config/routes.js
+++ config/routes.js
@@ -1,19 +1,9 @@
 miModulo.config(['$routeProvider',function($routeProvider){
 
-<<<<<<< HEAD
-    $routeProvider.when('/home',{templateUrl:'app/homeTemplate.html',controller:'homeController'});
-    $routeProvider.when('/plist',{templateUrl:'app/post/plist/plistTemplate.html',controller:'plistController'});
-    $routeProvider.otherwise({redirectTo:'/home'});
-=======
     $routeProvider.when('/',{templateUrl:'app/homeTemplate.html',controller:'homeController'})
-    
-    $routeProvider.when('/multi',{templateUrl:'app/multiplicacionTemplate.html',controller:'multiplicacionController'})
-    $routeProvider.when('/suma',{templateUrl:'app/sumaTemplate.html',controller:'sumaController'})
-
     $routeProvider.when('/post/plist/:rpp/:page',{templateUrl:'app/post/plist.html',controller:'postPlistController'})
 
     $routeProvider.otherwise({redirectTo:'/'})
->>>>>>> upstream/master
 
 
 }])
