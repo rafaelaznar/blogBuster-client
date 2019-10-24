68c7584cba5f448149b096d178d51f85ae2f16ca
pixeled4life <cozahmiah2@gmail.com>
2019-10-24 21:39:39 +0200

    Terminada la botonera
    Añadida la vecindad leida de una variable
    Cambiado selector de rpp para que se lea de un array
    Hechos calculos para llevarte a la pagina correcta cuando cambias los rpp
    Añadido .gitignore para la configuracion del IDE

--- app/post/plist/plist.js
+++ app/post/plist/plist.js
@@ -1,8 +1,10 @@
 var miControlador = miModulo.controller(
     "postPlistController",
     ['$scope', '$http', 'miServicio01', '$routeParams', function ($scope, $http, myService, $routeParams) {
-        $scope.paginaActual = $routeParams.page;
-        $scope.rppActual = $routeParams.rpp;
+        $scope.paginaActual = parseInt($routeParams.page);
+        $scope.rppActual = parseInt($routeParams.rpp);
+        $scope.rppS = [10,50,100];
+
 
         $http({
             method: 'GET',
@@ -20,33 +22,28 @@ var miControlador = miModulo.controller(
             $scope.status = response.data.status;
             $scope.numRegistros = response.data.response;
             $scope.numPaginas = Math.ceil($scope.numRegistros / $routeParams.rpp);
-            paginacion();
+            $scope.calcPage = [];
+            for (const p of $scope.rppS) {
+                const res = $scope.paginaActual/$scope.numPaginas;
+                const next = Math.ceil($scope.numRegistros / p);
+                $scope.calcPage.push(Math.round(res * next));
+            }
+            paginacion(2);
         }, function () {
         })
 
-        paginacion = function () {
+        function paginacion(vecindad) {
+            vecindad++;
             $scope.botonera = [];
             for (i = 1; i <= $scope.numPaginas; i++) {
                 if (i == 1) {
                     $scope.botonera.push(i);
-                }
-
-
-                if (i == $scope.paginaActual && i-1> 1) {
-                    // $scope.botonera.push(0);
-                    $scope.botonera.push(i-1);
-                }
-
-                if (i == $scope.paginaActual && i != 1) {
+                } else if (i > ($scope.paginaActual - vecindad) && i < ($scope.paginaActual + vecindad)) {
                     $scope.botonera.push(i);
-                }
-                if (i == $scope.paginaActual && i+1< $scope.numPaginas) {
-                    $scope.botonera.push(i+1);
-                    // $scope.botonera.push(0);
-                }
-
-                if (i == $scope.numPaginas && i != 1 && i != $scope.paginaActual) {
+                } else if (i == $scope.numPaginas) {
                     $scope.botonera.push(i);
+                } else if (i == ($scope.paginaActual - vecindad) || i == ($scope.paginaActual + vecindad)){
+                    $scope.botonera.push('...');
                 }
             }
         }
