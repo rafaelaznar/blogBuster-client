4dde57262faac002d264e3e947728fe5d2ea9de0
Rafael Aznar <rafaaznar@gmail.com>
2019-11-04 09:51:09 +0100

    Home page

--- app/homeController.js
+++ app/homeController.js
@@ -1,11 +1,63 @@
 var miControlador = miModulo.controller(
     "homeController",
-    ['$scope', '$http', 'auth',
-        function ($scope, $http, auth) {
+    ['$scope', '$http', 'auth', '$location', '$routeParams',
+        function ($scope, $http, auth, $location, $routeParams) {
+            if (auth.data.status != 200) {
+                $location.path('/login');
+            }
             $scope.authStatus = auth.data.status;
             $scope.authUsername = auth.data.message;
 
             $scope.controller = "homeController";
+
+            $scope.paginaActual = parseInt($routeParams.page);
+            $scope.rppActual = parseInt($routeParams.rpp);
+            $scope.rppS = [10, 50, 100];
+            $scope.controller = "postPlistController";
+
+            $http({
+                method: 'GET',
+                url: 'http://localhost:8081/blogbuster/json?ob=post&op=getpage&rpp=' + $routeParams.rpp + '&page=' + $routeParams.page
+            }).then(function (response) {
+                $scope.status = response.data.status;
+                $scope.pagina = response.data.message;
+            }, function () { })
+
+            $http({
+                method: 'GET',
+                url: 'http://localhost:8081/blogbuster/json?ob=post&op=getcount'
+            }).then(function (response) {
+                $scope.status = response.data.status;
+                $scope.numRegistros = response.data.message;
+                $scope.numPaginas = Math.ceil($scope.numRegistros / $routeParams.rpp);
+                $scope.calcPage = [];
+                for (const p of $scope.rppS) {
+                    const res = $scope.paginaActual / $scope.numPaginas;
+                    const next = Math.ceil($scope.numRegistros / p);
+                    $scope.calcPage.push(Math.round(res * next));
+                }
+                paginacion(2);
+            }, function () { })
+
+            function paginacion(vecindad) {
+                vecindad++;
+                $scope.botonera = [];
+                for (i = 1; i <= $scope.numPaginas; i++) {
+                    if (i == 1) {
+                        $scope.botonera.push(i);
+                    } else if (i > ($scope.paginaActual - vecindad) && i < ($scope.paginaActual + vecindad)) {
+                        $scope.botonera.push(i);
+                    } else if (i == $scope.numPaginas) {
+                        $scope.botonera.push(i);
+                    } else if (i == ($scope.paginaActual - vecindad) || i == ($scope.paginaActual + vecindad)) {
+                        $scope.botonera.push('...');
+                    }
+                }
+            }
+
+
+
+
         }
     ]
 )
